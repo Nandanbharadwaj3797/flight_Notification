04b033dafe3ade4a72d1cac99644026060ac8260
nandan kumar <nandanbharadwaj4@gmail.com>
2025-07-24 20:28:47 +0530

    completed consumption and sending mail

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -1,9 +1,8 @@
-const dotenv=require('dotenv');
-
+const dotenv = require('dotenv');
 dotenv.config();
 
-module.exports={
+module.exports = {
   PORT: process.env.PORT || 3000,
-  GMAIL_PASS:process.env.GMAIL_PASSWORD ,
-  GMAIL_EMAIL:process.env.GMAIL_EMAIL,
-}
+  GMAIL_PASS: process.env.GMAIL_PASSWORD,
+  GMAIL_EMAIL: process.env.GMAIL_EMAIL
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 const express = require('express');
+const amqplib = require('amqplib');
+const{EmailService} = require('./services');
 
-const {serverConfig, Logger }= require('./config');
+async function connecttoQueue() {
+  try {
+    const connection = await amqplib.connect('amqp://localhost');
+    const channel = await connection.createChannel();
+    const queueName = 'noti-queue';
+    await channel.assertQueue(queueName, { durable: true });
+    channel.consume(queueName, async(data)=>{
+      console.log(`${Buffer.from(data.content)}`);
+      const obj=JSON.parse(`${Buffer.from(data.content)}`);
+      await EmailService.sendEmail("codingtestmail1@gmail.com", obj.recepientEmail, obj.subject, obj.content);
+      channel.ack(data);
+    })
+  } catch (error) {
+    console.error('Error connecting to RabbitMQ:', error);
+  }
+}
+
+const serverConfig = require('./config');
+const mailsender= require('./config/email-config');
+
 const apiRoutes = require('./routes');
 
 const app = express();
@@ -16,5 +37,6 @@ app.use('/api',apiRoutes);
 
 app.listen(serverConfig.PORT, async() => {
   console.log(`Server is running on port ${serverConfig.PORT}`);
-  
+  await connecttoQueue();
+  console.log('Connected to RabbitMQ up');
 });
